b1a4bd995ecfdadae4c45facdcefa7b3aaa4f93a
Sds0702 <seq.sam@gmail.com>
2023-05-31 11:09:59 +0100

    Add project files.

---
+++ IService1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceTempConvertion
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        double CelsiustoFahrenheit(double CTemp);
+        [OperationContract]
+        double CelsiustoKelvin(double CTemp);
+        [OperationContract]
+        double FahrenheittoCelsius(double FTemp);
+        [OperationContract]
+        double FahrenheittoKelvin(double FTemp);
+        [OperationContract]
+        double KelvintoCelsius(double KTemp);
+        [OperationContract]
+        double KelvintoFahrenheit(double KTemp);
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceTempConvertion
+{
+        public class Service1 : IService1
+    {
+        public double CelsiustoFahrenheit(double CTemp)
+        {
+            double f = (CTemp * 1.8) + 32;
+            return f;
+        }
+        public double CelsiustoKelvin(double CTemp)
+        {
+            double k = CTemp + 273.15;
+            return k;
+        }
+        public double FahrenheittoCelsius(double FTemp)
+        {
+            double c = (FTemp - 32) * 0.55;
+            return c;
+        }
+        public double FahrenheittoKelvin(double FTemp)
+        {
+            double k = (FTemp - 32) * 0.55 + 273.15;
+            return k;
+        }
+        public double KelvintoCelsius(double KTemp)
+        {
+            double c = KTemp - 273.15;
+            return c;
+        }
+        public double KelvintoFahrenheit(double KTemp)
+        {
+            double f = (KTemp - 273.15) * 0.55 + 273.15;
+            return f;
+        }
+    }
+}
